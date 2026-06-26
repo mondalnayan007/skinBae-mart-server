9d95f0e27e1aa9c06ec76cdb72b281d25baf200f
Nayan Mondal <n007kingkong@gmail.com>
2026-06-26 23:11:36 +0600

    implemented the api for getting single product

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -37,14 +37,28 @@ async function run() {
         const productsCollection = db.collection('products')
 
 
-
+    //   get all products 
        app.get('/products',async(req,res)=>{
             
         const cursor = productsCollection.find();
         const result = await cursor.toArray();
         res.send(result);
 
-       })
+       });
+
+    //  get single product by id 
+
+
+    app.get('/products/:id' , async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await productsCollection.findOne(query);
+        res.send(result);
+    })
+
+
+
+
 
 
 

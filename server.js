019589f8ac7e61eb344095094467cd258576ed91
Nayan Mondal <n007kingkong@gmail.com>
2026-06-26 22:20:16 +0600

    server is running successfully

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
